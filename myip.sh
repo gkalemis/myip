3933fe6e9c57a18d3c81464e9e7f0795f7ca91ae
George Kalemis <george.kalemis.edu@gmail.com>
2025-12-14 10:31:09 +0200

    Refactor IP retrieval and logging in myip.sh

--- myip.sh
+++ myip.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 # Specify the filename
-FILE="$HOME/useful/scripts/myip/ips2.txt"
-COMMAND="$(external-ip dns)"
-ip="194.63.236.15"
-max_retries=5
-attempt=1
-previous_ip=$(tail -n 1 "$FILE" | awk -F' - ' '{print $2}')
+FILE="$HOME/scripts/myip/ips.txt"
 
 # Check if file exists
 if [ ! -f "$FILE" ]; then
@@ -14,17 +9,37 @@ if [ ! -f "$FILE" ]; then
     touch "$FILE"
 fi
 
+#echo "Previous ip was: $last_line"
+previous_ip=$(tail -n 1 "$FILE" | awk -F' - ' '{print $2}')
 
-while ! $COMMAND >/dev/null 2>&1; do
-    ((attempt++))
-    if [ "$attempt" -gt "$max_retries" ]; then
-    	exit 1
+#echo "Timestamp is: $timestamp"
+timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+
+max_attempts=5
+attempt=0
+
+while [[ $attempt -lt $max_attempts ]]; do
+    #echo "Current ip is: $result"
+    current_ip=$(dig +short txt ch whoami.cloudflare @1.0.0.1 | tr -d '"')
+    if [[ $current_ip == *"error"* ]]; then
+        current_ip=""
     fi
-    sleep 9 
+    if [ -n "$current_ip" ]; then
+        echo "Current ip is: $current_ip"
+        break  # Exit the loop if the ping is successful
+    else
+        echo "Current ip cannot be found after $((attempt + 1)) attempts. Retrying in 7 seconds..."
+        ((attempt++))
+        sleep 7
+    fi
 done
 
-if $COMMAND >/dev/null 2>&1; then
-	if [[ "$previous_ip" != "$COMMAND" ]]; then
-       		echo "$timestamp - $current_ip"  
-	fi
+if [[ $attempt -eq $max_attempts ]]; then
+    echo "Current ip cannot be found after $max_attempts attempts."
+    exit 1  # Exit the script if all attempts fail
+else
+        # Write the new ip to the file
+        if [[ "$previous_ip" != "$current_ip" ]]; then
+                echo "$timestamp - $current_ip" >> $FILE
+        fi
 fi
